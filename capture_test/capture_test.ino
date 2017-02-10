4933c74c259ba2c42fdb8bc8fd34c6d8112d6c38
fundamelon <igi.c@att.net>
2017-02-10 02:25:47 -0800

    added data filtering; increased data precision

--- capture_test/capture_test.ino
+++ capture_test/capture_test.ino
@@ -22,10 +22,10 @@ const uint8_t channelmap[] = {3, 1, 0, 2, 4, 5};
 const uint8_t PID_MAX = 70;
 
 // PID tuning values
-double ch1_kp = 3, ch1_ki = 0.9, ch1_kd = 0.5;
-double ch2_kp = 0.3, ch2_ki = 0.2, ch2_kd = 1;
-double ch3_kp = ch2_kp, ch3_ki = ch2_ki, ch3_kd = ch2_kd; // same moment of inertia on X and Y axes.
-double ch4_kp = 1, ch4_ki = 0.5, ch4_kd = 0.25;
+double ch1_kp = 0.05, ch1_ki = 0.01, ch1_kd = 0.1;
+double ch2_kp = 0.8, ch2_ki = 0.4, ch2_kd = 0.3;
+double ch3_kp = 1, ch3_ki = 0.5, ch3_kd = 0.2;
+double ch4_kp = 2, ch4_ki = 0, ch4_kd = 0;
 
 // PID variables
 double ch1_setpoint, ch1_input, ch1_output;
@@ -34,22 +34,52 @@ double ch3_setpoint, ch3_input, ch3_output;
 double ch4_setpoint, ch4_input, ch4_output;
 
 // PID objects
-PID ch1_pid(&ch1_input, &ch1_output, &ch1_setpoint, ch1_kp, ch1_ki, ch1_kd, DIRECT);
+PID ch1_pid(&ch1_input, &ch1_output, &ch1_setpoint, ch1_kp, ch1_ki, ch1_kd, REVERSE);
 PID ch2_pid(&ch2_input, &ch2_output, &ch2_setpoint, ch2_kp, ch2_ki, ch2_kd, REVERSE);
 PID ch3_pid(&ch3_input, &ch3_output, &ch3_setpoint, ch3_kp, ch3_ki, ch3_kd, DIRECT);
-PID ch4_pid(&ch4_input, &ch4_output, &ch4_setpoint, ch4_kp, ch4_ki, ch4_kd, DIRECT);
+PID ch4_pid(&ch4_input, &ch4_output, &ch4_setpoint, ch4_kp, ch4_ki, ch4_kd, REVERSE);
 
 // types of data being sent
 const int DATA_START_VAL = 7777;
 const int DATA_END_VAL = 9999;
 enum DATA_TYPES {DATA_START, DATA_MODE, DATA_POS_X, DATA_POS_Y, DATA_HDG, DATA_ALT, DATA_END};
 
+// state which denotes which information is next
+int next_data = DATA_START;
+
 // modes of operation
 const uint8_t MODE_PASSTHRU = 0;
 const uint8_t MODE_CAPTURE = 1;
 
+// mode logic variables
+uint8_t mode = MODE_PASSTHRU;
+uint8_t mode_requested;
+uint8_t flag_mode_change = 0;
+
 // voltage scale for RDAC
 float vscale = 0.0f;
+
+// pre-filter values
+float pos_x_pre = 0.0f, pos_y_pre = 0.0f, heading_pre = 0.0f, altitude_pre = 0.0f;
+// filtered inputs
+float pos_x = 0.0f, pos_y = 0.0f, heading = 0.0f, altitude = 0.0f;
+
+// calibration variables
+uint8_t flag_calibrated = 0;
+uint8_t calibration_stage = 0;
+
+unsigned long time_capture_lost = 0;
+uint8_t flag_capture_lost = 0;
+unsigned long capture_timeout = 1000; // wait 1000 milliseconds to resume manual on capture loss
+
+// input filtering
+uint8_t enable_filtering = 0;
+uint8_t flag_data_received = 0;
+const uint8_t data_filter_max = 4;
+float data_filter[4][data_filter_max] = { 0.0f };
+uint8_t data_filter_index = 0;
+uint8_t data_filter_size = 0;
+
 
 void setup() {
   // SPI for RDAC
@@ -63,7 +93,7 @@ void setup() {
   pinMode(LED_MODE, OUTPUT);
 
   // setup PID output limits
-  ch1_pid.SetOutputLimits(0, PID_MAX);
+  ch1_pid.SetOutputLimits(-20, 20);
   ch2_pid.SetOutputLimits(-PID_MAX, PID_MAX);
   ch3_pid.SetOutputLimits(-PID_MAX, PID_MAX);
   ch4_pid.SetOutputLimits(-PID_MAX, PID_MAX);
@@ -78,23 +108,10 @@ void setup() {
   vscale = 3/5;
 
   // blink ready
+  Serial.println("Guidance module ready");
   for(int i = 0; i < 6; i++) { digitalWrite(LED_MODE, (i % 2) ? HIGH : LOW); delay(150); }
 }
 
-int next_data = DATA_START;
-int pos_x = 0, pos_y = 0, heading = 0, altitude = 0;
-
-uint8_t mode = MODE_PASSTHRU;
-uint8_t mode_requested;
-uint8_t flag_mode_change = 0;
-
-// calibration variables
-uint8_t flag_calibrated = 0;
-uint8_t calibration_stage = 0;
-
-unsigned long time_capture_lost = 0;
-uint8_t flag_capture_lost = 0;
-unsigned long capture_timeout = 1000; // wait 1000 milliseconds to resume manual on capture loss
 
 void loop() {
   uint8_t inputs[8];
@@ -104,20 +121,23 @@ void loop() {
   for(uint8_t i = 0; i < 8; i++) {
     inputs[i] = (uint8_t)(analogRead(PIN_CH1 + i) >> 2);
   }
-  
+
+  // voltage scaling
   vscale = ((float)analogRead(A5) / (float)analogRead(A4));
 
   // receive instructions
   if(Serial.available() > 0) {
-    int data = (int)Serial.parseInt();
     //Serial.println(data);
+    int data;
     switch(next_data) {
       case DATA_START:
+        data = (int)Serial.parseInt();
         if(data != DATA_START_VAL) break;
         next_data = DATA_MODE;
         break;
       case DATA_MODE:
         // mode changes are request-based
+        data = (int)Serial.parseInt();
         if(data != mode) {
           if(mode == MODE_CAPTURE) {
             if(time_capture_lost == 0) {
@@ -138,27 +158,69 @@ void loop() {
         next_data = DATA_POS_X;
         break;
       case DATA_POS_X:
-        pos_x = data;
+        digitalWrite(LED_MODE, HIGH);
+        pos_x_pre = Serial.parseFloat();
+        digitalWrite(LED_MODE, LOW);
         next_data = DATA_POS_Y;
         break;
       case DATA_POS_Y:
-        pos_y = data;
+        pos_y_pre = Serial.parseFloat();
         next_data = DATA_HDG;
         break;
       case DATA_HDG:
-        heading = data;
+        heading_pre = Serial.parseFloat();
         next_data = DATA_ALT;
         break;
       case DATA_ALT:
-        altitude = data;
+        altitude_pre = Serial.parseFloat();
+        flag_data_received = 1;
         next_data = DATA_END;
+        break;
       case DATA_END:
+        data = (int)Serial.parseInt();
         if(data != DATA_END_VAL) break;
         next_data = DATA_START;
         break;
       default:
+        data = (int)Serial.parseInt();
+        Serial.print("JUNK VALUES >> ");
+        Serial.println(data);
         break;
     }
+  }
+
+  // input filtering
+  if(enable_filtering && flag_data_received) {
+    // store raw inputs
+    uint8_t j = data_filter_index;
+    data_filter[0][j] = pos_x_pre;
+    data_filter[1][j] = pos_y_pre;
+    data_filter[2][j] = heading_pre;
+    data_filter[3][j] = altitude_pre;
+
+    if(data_filter_size < 10) data_filter_size++;
+    data_filter_index = (data_filter_index + 1) % data_filter_max;
+
+    // compute average of stored inputs
+    pos_x = pos_y = heading = altitude = 0.0f;
+    for(uint8_t i = 0; i < data_filter_size; i++) {
+      pos_x += data_filter[0][i];
+      pos_y += data_filter[1][i];
+      heading += data_filter[2][i];
+      altitude += data_filter[3][i];
+    }
+
+    pos_x /= data_filter_size;
+    pos_y /= data_filter_size;
+    heading /= data_filter_size;
+    altitude /= data_filter_size;
+    
+    flag_data_received = 0;
+  } else if(!enable_filtering) {
+    pos_x = pos_x_pre;
+    pos_y = pos_y_pre;
+    heading = heading_pre;
+    altitude = altitude_pre;
   }
 
   // TODO: more complex mode change logic
@@ -208,21 +270,23 @@ void loop() {
       flag_mode_change = 0;
     }
 
+    uint8_t target_altitude = 60;
+
     // pid sensor inputs
     if(!flag_capture_lost) {
-      float hdg_rad = (heading * 71) / 4068;
+      float hdg_rad = (heading * 3.1415) / 180.0;
       
-      ch1_input = altitude;
-      ch2_input = pos_x;// * sin(heading) + pos_y * cos(heading);
-      ch3_input = pos_y;// * sin(heading) + pos_x * cos(heading);
+      ch1_input = altitude - target_altitude;
+      ch2_input = pos_x;// * sin(hdg_rad) + pos_y * cos(hdg_rad);
+      ch3_input = pos_y;// * sin(hdg_rad) + pos_x * cos(hdg_rad);
       ch4_input = heading;
     }
 
     // pid setpoints (targets)
-    ch1_setpoint = 60;
+    ch1_setpoint = target_altitude;
     ch2_setpoint = 0;
     ch3_setpoint = 0;
-    ch4_setpoint = 45;
+    ch4_setpoint = 0;
 
     // crunch the pids
     ch1_pid.Compute();
@@ -231,10 +295,14 @@ void loop() {
     ch4_pid.Compute();
 
     // read out pid outputs
-    control_inputs[0] = (uint8_t)ch1_output;
+    control_inputs[0] = (uint8_t)constrain(round(ch1_output + inputs[0]), 0, 255);
     control_inputs[1] = (uint8_t)constrain(round(ch2_output + inputs[1]), 0, 255);
     control_inputs[2] = (uint8_t)constrain(round(ch3_output + inputs[2]), 0, 255);
     control_inputs[3] = (uint8_t)constrain(round(ch4_output + inputs[3]), 0, 255);
+
+    // throttle cut override
+    if(inputs[0] < 10) control_inputs[0] = inputs[0];
+    
     /*
     // write to RDAC
     for(uint8_t i = 0; i < 4; i++) {
@@ -247,10 +315,11 @@ void loop() {
     digitalPotWrite(channelmap[3], 255 - (uint8_t)(inputs[3] * vscale));
 
     // serial debug
-    Serial.print("X: ");
-    Serial.println(pos_x);
-    Serial.print("CH2: ");
-    Serial.println(ch2_output);
+  
+    //Serial.print("ALT: ");
+    //Serial.println(altitude - target_altitude);
+    //Serial.print("CH1: ");
+    //Serial.println(ch1_output);
 
     if(flag_capture_lost)
       digitalWrite(LED_MODE, (millis() / 100 % 2) ? HIGH : LOW);
