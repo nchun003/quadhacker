bf57846fcb163a7da189712ab3a6af49be9e25b7
Nichelle Chung <nchun003@ucr.edu>
2017-02-25 14:05:59 -0800

    Modified with radio library

--- Receiver.ino
+++ Receiver.ino
@@ -1,5 +1,22 @@
+//#include <Radio.h>
+
+//#include <cc1101.h>
+//#include <ccpacket.h>
+//#include <commonregs.h>
+//#include <nvolat.h>
+//#include <panstamp.h>
+//#include <register.h>
+//#include <spi.h>
+//#include <swcommand.h>
+//#include <swpacket.h>
+//#include <swquery.h>
+//#include <swstatus.h>
+
 #include "EEPROM.h"
 #include "cc1101.h"
+#include "Radio.h"
+
+//MEGA
 
 // The LED is wired to the Arduino Output 4 (physical panStamp pin 19)
 #define LEDOUTPUT 4
@@ -7,15 +24,17 @@
 // The connection to the hardware chip CC1101 the RF Chip
 CC1101 cc1101;
 
-byte b;
-byte i;
-byte syncWord = 199;
-long counter=0;
-byte chan=0;
+//byte b;
+//byte i;
+//byte syncWord = 199;
+//long counter=0;
+//byte chan=0;
+Radio radio(byte(0),byte(0),byte(199),long(0),byte(0));
+
 
 // a flag that a wireless packet has been received
 boolean packetAvailable = false;
-
+//
 void blinker(){
 digitalWrite(LEDOUTPUT, HIGH);
 delay(100);
@@ -23,11 +42,13 @@ digitalWrite(LEDOUTPUT, LOW);
 delay(100);
 }
 
+
 /* Handle interrupt from CC1101 (INT0) gdo0 on pin2 */
 void cc1101signalsInterrupt(void){
 // set the flag that a package is available
 packetAvailable = true;
 }
+//cc1101signalsInterrupt(void);
 
 void setup()
 {
@@ -40,12 +61,14 @@ digitalWrite(LEDOUTPUT, LOW);
 
 // blink once to signal the setup
 blinker();
+//radio.blinker();
 Serial.println("at Blinker()");
 // initialize the RF Chip
 cc1101.init();
 Serial.println("at init()");
 
-cc1101.setSyncWord(&syncWord, false);
+byte bla = radio.get_syncWord();
+cc1101.setSyncWord(&bla, false);
 cc1101.setCarrierFreq(CFREQ_915);
 cc1101.disableAddressCheck(); //if not specified, will only display "packet received"
 //cc1101.setTxPowerAmp(PA_LowPower);
@@ -74,57 +97,57 @@ Serial.println(value);
 
 void ReadRSSI()
 {
-byte rssi=0;
-byte value=0;
-
-rssi=(cc1101.readReg(CC1101_RSSI, CC1101_STATUS_REGISTER));
-
-if (rssi >= 128)
-{
-value = 255 - rssi;
-value /= 2;
-value += 74;
-}
-else
-{
-value = rssi/2;
-value += 74;
-}
-Serial.print("CC1101_RSSI ");
-Serial.println(value);
+  byte rssi=0;
+  byte value=0;
+  
+  rssi=(cc1101.readReg(CC1101_RSSI, CC1101_STATUS_REGISTER));
+  
+  if (rssi >= 128)
+  {
+    value = 255 - rssi;
+    value /= 2;
+    value += 74;
+  }
+  else
+  {
+    value = rssi/2;
+    value += 74;
+  }
+  Serial.print("CC1101_RSSI ");
+  Serial.println(value);
 }
 
 void loop()
 {
-if(packetAvailable){
-Serial.println("packet received");
-// Disable wireless reception interrupt
-detachInterrupt(0);
-
-ReadRSSI();
-ReadLQI();
-// clear the flag
-packetAvailable = false;
-
-CCPACKET packet;
-
-if(cc1101.receiveData(&packet) > 0){
-if(!packet.crc_ok) {
-Serial.println("crc not ok");
-}
-
-if(packet.length > 0){
-Serial.print("packet: len ");
-Serial.print(packet.length);
-Serial.print(" data: ");
-for(int j=0; j<packet.length; j++){
-Serial.print(packet.data[j],HEX);
-Serial.print(" ");
-}
-Serial.println(".");
-}
-}
-// Enable wireless reception interrupt
-attachInterrupt(0, cc1101signalsInterrupt, FALLING);
-}
+  if(packetAvailable){
+    Serial.println("packet received");
+    // Disable wireless reception interrupt
+    detachInterrupt(0);
+    
+    ReadRSSI();
+    ReadLQI();
+    // clear the flag
+    packetAvailable = false;
+    
+    CCPACKET packet;
+    
+    if(cc1101.receiveData(&packet) > 0){
+      if(!packet.crc_ok) {
+      Serial.println("crc not ok");
+      }
+    
+      if(packet.length > 0){
+        Serial.print("packet: len ");
+        Serial.print(packet.length);
+        Serial.print(" data: ");
+        for(int j=0; j<packet.length; j++){
+          Serial.print(packet.data[j],HEX);
+          Serial.print(" ");
+        }
+        Serial.println(".");
+      }
+    }
+    // Enable wireless reception interrupt
+    attachInterrupt(0, cc1101signalsInterrupt, FALLING);
+  }
 }
