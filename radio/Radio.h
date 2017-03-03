3a926a32a17fba5f37e4b8743a7f7748e22f8e15
Nichelle Chung <nchun003@ucr.edu>
2017-03-03 01:11:07 -0800

    Functions added

--- radio/Radio.h
+++ radio/Radio.h
@@ -9,25 +9,24 @@ class Radio
   public:
 	//Radio(byte b, byte i, byte syncWord, long counter, byte chan);
 	Radio();
-    void read_rpi_data();
-	long parseInt();
-	float parseFloat();
+	void init();
+    void update();
 	long available();
-	int read();
+	uint8_t read();
 	byte get_b();
 	byte get_i();
 	byte get_syncWord();
 	long get_counter();
 	byte get_chan();
-	void ReadLQI();
-	void ReadRSSI();
+	byte ReadLQI();
+	byte ReadRSSI();
+	CC1101 cc1101;
   private:
     byte _b;
     byte _i;
     byte _syncWord;
     long _counter;
     byte _chan;
-	CC1101 cc1101;
 };
 
 #endif
